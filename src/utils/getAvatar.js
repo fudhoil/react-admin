b3a0995d0d149eeed43540d340c1915b57f5695b
fudhoil <fudhoil.10119333@mahasiswa.unikom.ac.id>
2022-09-27 07:39:13 +0700

    avatar url

--- src/utils/getAvatar.js
+++ src/utils/getAvatar.js
@@ -1,6 +1,8 @@
 const { getCookie } = require("src/services/cookies");
 
 const user = getCookie("userProfile");
-const getAvatar = "https://gxoib8zz.directus.app/assets/" + user.data[0].avatar;
+const getAvatar = user
+  ? "https://gxoib8zz.directus.app/assets/" + user.data[0].avatar
+  : "src/assets/images/avatars/5.jpg";
 
 export default getAvatar;
